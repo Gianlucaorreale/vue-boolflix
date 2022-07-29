218dc5aa28276eef219a9062d47e0539a5b954c5
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-07-29 14:39:01 +0200

    milestone-1

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,55 @@
 <template>
-  <div id="app">
-    
-  </div>
+<div>
+  <SearchBar placeholder="cerca film.." @search="fetchMovies"/>
+  <section id="movies">
+    <h2>Movies</h2>
+    <ul v-for="movie in movies" :key="movie.id">
+       <li>{{movie.title}}</li>
+       <li>{{movie.original_title}}</li>
+       <li>{{movie.original_language}}</li>
+       <li>{{movie.vote_average}}</li>
+    </ul>
+  </section>
+</div>
 </template>
 
 <script>
-
-
+import axios from 'axios';
+import SearchBar from './components/SearchBar.vue';
 export default {
   name: 'App',
-  components: {
-   
+  components:{ SearchBar },
+  data(){
+    return{
+      movies:[],
+      api:{
+        language: 'it-IT',
+        key:'e3e416df7064ff3d8ef2cebfaadfaf00',
+        baseUri: 'https://api.themoviedb.org/3'
+      }
+    }
+  },
+  methods:{
+    fetchMovies(query){
+      const {language, key, baseUri} = this.api;
+
+      const config = {
+        params: {
+          api_key: key,
+          language,
+          query,
+        }
+      } 
+
+      axios.get(`${baseUri}/search/movie`,config).then((res)=>{
+         this.movies = res.data.results
+      })
+    }
   }
 }
 </script>
 
-<style lang="scss">
+
+<style>
 
 </style>
