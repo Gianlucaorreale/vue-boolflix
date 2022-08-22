7956f31ea7ae6386adcfeaeb46e90edda045bc34
Gianlucaorreale <gianlucacorreale.1996@gmail.com>
2022-08-22 17:10:37 +0200

    added-MILESTONE-3

--- src/components/ProductionCard.vue
+++ src/components/ProductionCard.vue
@@ -6,6 +6,10 @@
         <img v-if="hasFlag" :src="flagSrc" :alt="production.original_language"/>
        <span v-else>{{production.original_language}}</span> 
         </li>
+        <li>
+          <FontAwsomeIcon v-for="n in 5" :key="n" :icon="renderStar(n)" />
+          {{ vote }}
+          </li>
        <li><img :src="cover" :alt="title"></li>
        
     </ul>
@@ -13,6 +17,7 @@
 
 <script>
 const posterUrlRoot = 'https://image.tmdb.org/t/p/w342';
+const coverPlaceholder = 'https://www.altavod.com/assets/images/poster-placeholder.png';
 export default{
   name:'ProductionCard',
   
@@ -20,7 +25,11 @@ export default{
     production: Object,
   },
   computed:{
+    vote(){
+      return Math.ceil(this.production.vote_average / 2)
+    },
     cover(){
+      if(!this.production.poster_path) return coverPlaceholder;
       return  posterUrlRoot + this.production.poster_path;
     },
     hasFlag(){
@@ -31,6 +40,12 @@ export default{
         return require(`../assets/img/flags/${this.production.original_language}.png`)
     },
   },
+  methods: {
+    renderStar(n){
+      const iconType = this.vote >= n ? "fa-solid": "fa-regular";
+      return iconType + "fa-star";
+    }
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faStar as faStarSolid } from '@fortawesome/free-solid-svg-icons'
+import { faStar as faStarRegular } from '@fortawesome/free-regular-svg-icons'
+
+
+library.add(faStarSolid, faStarRegular);
+Vue.component('FontAwsomeIcon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
